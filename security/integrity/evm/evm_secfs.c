d596775e87a5de2a55a6c62849055a629fb13a77
Janusz Kozerski <j.kozerski@samsung.com>
2014-04-02 16:10:27 +0200

    evm: add interface to read and write EVM state (ENABLE/DISABLE/FIX).
    The state of EVM can be change by writing to the <securityfs>/evm_state,
    and can be check by reading from the interface.
    To change the state of EVM write:
    0 - for disabling
    1 - for enabling
    2 - for FIX mode
    
    Notice that evm have to be initialized before you use the interface.
    
    To enable this option you have to set a proper option in Kconfig.
    
    Change-Id: Id68546fcae3c32087b85314da863a0c5bd9e1a94
    
    Signed-off-by: Janusz Kozerski <j.kozerski@samsung.com>

--- security/integrity/evm/evm_secfs.c
+++ security/integrity/evm/evm_secfs.c
@@ -29,19 +29,40 @@ static struct dentry *evm_init_tpm;
  * @count: maximum to send along
  * @ppos: where to start
  *
+ * If EVM_STATE_INTERFACE is enabled this should write to the userspace:
+ * 0 - when EVM is disabled or uninitialized
+ * 1 - when EVM is in enforce mode (initialized and enabled)
+ * 2 - when EVM is in FIX mode (initialized and fix)
+ * To use this interface user needs to have CAP_SYS_ADMIN
+ * Notice that the param "evm=fix" has higher priority, and if it was passed
+ * to the kernel then EVM will be running in FIX mode (or will be disabled).
+ *
+ * If EVM_STATE_INTERFACE is disabled it writes:
+ * 0 - when EVM is uninitialized
+ * 1 - when EVM is initialized
+ *
  * Returns number of bytes read or error code, as appropriate
  */
 static ssize_t evm_read_key(struct file *filp, char __user *buf,
 			    size_t count, loff_t *ppos)
 {
-	char temp[80];
+	char temp[2] = {'0', '\0'};
 	ssize_t rc;
 
 	if (*ppos != 0)
 		return 0;
 
-	sprintf(temp, "%d", evm_initialized);
-	rc = simple_read_from_buffer(buf, count, ppos, temp, strlen(temp));
+#ifdef CONFIG_EVM_STATE_INTERFACE
+	if (!capable(CAP_SYS_ADMIN))
+		return -EPERM;
+
+	if (evm_initialized && evm_enabled)
+		temp[0] += evm_fixmode ? 2 : 1;
+#else  /* CONFIG_EVM_STATE_INTERFACE */
+	temp[0] += evm_initialized;
+#endif /* CONFIG_EVM_STATE_INTERFACE */
+
+	rc = simple_read_from_buffer(buf, count, ppos, temp, sizeof(temp));
 
 	return rc;
 }
@@ -64,7 +85,7 @@ static ssize_t evm_write_key(struct file *file, const char __user *buf,
 	char temp[80];
 	int i;
 
-	if (!capable(CAP_SYS_ADMIN) || (evm_initialized & EVM_STATE_KEY_SET))
+	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
 
 	if (count >= sizeof(temp) || count == 0)
@@ -75,10 +96,32 @@ static ssize_t evm_write_key(struct file *file, const char __user *buf,
 
 	temp[count] = '\0';
 
-	if ((sscanf(temp, "%d", &i) != 1) || (i != 1))
+	if ((sscanf(temp, "%d", &i) != 1) || ((i != 1)
+#ifdef CONFIG_EVM_STATE_INTERFACE
+			&& (i != 0) && (i != 2)
+#endif /* CONFIG_EVM_STATE_INTERFACE */
+			))
 		return -EINVAL;
 
-	evm_init_key();
+	if (evm_initialized == 0) {
+		if (evm_init_key())
+			/* If EVM cannot be initialized
+			 * no more action should be taken */
+			return count;
+	}
+
+	if (i == 1) {
+		evm_enabled = 1;
+#ifdef CONFIG_EVM_STATE_INTERFACE
+		if (!evm_forcefix)
+			evm_fixmode = 0;
+	} else if (i == 0)
+		evm_enabled = 0;
+	else if (i == 2) {
+		evm_enabled = 1;
+		evm_fixmode = 1;
+#endif /* CONFIG_EVM_STATE_INTERFACE */
+	}
 
 	return count;
 }
@@ -96,5 +139,6 @@ int __init evm_init_secfs(void)
 					      NULL, NULL, &evm_key_ops);
 	if (!evm_init_tpm || IS_ERR(evm_init_tpm))
 		error = -EFAULT;
+
 	return error;
 }
